492f9390a63a015c6001e9957cbf8c5b5933f104
haozhang <hzhang428@gatech.edu>
2017-04-14 19:16:02 -0400

    drawer toggle display and animation

--- app/src/main/java/com/example/haozhang/dabblin/MainActivity.java
+++ app/src/main/java/com/example/haozhang/dabblin/MainActivity.java
@@ -1,13 +1,100 @@
 package com.example.haozhang.dabblin;
 
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer) NavigationView navigationView;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        setupDrawer();
+
+
+    }
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            /**
+             * Called when an item in the navigation menu is selected.
+             *
+             * @param item The selected item
+             * @return true to display the item as the selected item
+             */
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.isChecked()) {
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+                switch (item.getItemId()) {
+                    case R.id.drawer_menu_home:
+                        Toast.makeText(MainActivity.this, "Home clicked", Toast.LENGTH_LONG).show();
+                        setTitle(R.string.drawer_menu_home);
+                        break;
+                    case R.id.drawer_menu_likes:
+                        Toast.makeText(MainActivity.this, "Likes clicked", Toast.LENGTH_LONG).show();
+                        setTitle(R.string.drawer_menu_likes);
+                        break;
+                    case R.id.drawer_menu_buckets:
+                        Toast.makeText(MainActivity.this, "buckets clicked", Toast.LENGTH_LONG).show();
+                        setTitle(R.string.drawer_menu_buckets);
+                        break;
+                }
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
